474c32c63cb79f427fe1990e141abcea8e9c2b6e
Yael <yael.mattmann@gmail.com>
2022-10-30 15:24:06 +0000

    Added Title method, Title test pass and Title test fail

--- src/main/java/atu/ie/Passenger.java
+++ src/main/java/atu/ie/Passenger.java
@@ -1,18 +1,29 @@
 package atu.ie;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Passenger {
     String passengerTitle, passengerName, passengerID,passengerPhone;   int passengerAge;
 
     public Passenger(String title,String name,String id,String phone,int age){      //Constructor
         passengerAge = age;
-
+        passengerTitle = title;
+        passengerName = name;
+        passengerID = id;
+        passengerPhone = phone;
     }
+
     public int Age(){
         if (passengerAge < 16){
             throw new IllegalArgumentException("Age must be over 16");
         }
         return passengerAge;
+    }
+
+    public String Title(){
+        if(passengerTitle != "Mr" && passengerTitle != "Mrs" && passengerTitle != "Ms"){
+            throw new IllegalArgumentException("Passenger title must be either Mrs, Mr or Ms");
+        }
+        return passengerTitle;
     }
 }
